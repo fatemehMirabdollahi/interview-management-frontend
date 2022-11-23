5bb6aa0a01e0f9a240cebf849168fc1c04dd9d1a
fmirabdollahi <f.mirabdollahi@samayesh.com>
2022-11-24 00:33:24 +0330

    add comments list to meet form

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -2,13 +2,13 @@
   <div class="meet i-flex-column" v-if="!showDetails">
     <div class="i-flex i-flex-justify-between">
       <div class="meet__field i-flex">
-        <span class="meet__lable"> نام و نام خانوادگی: </span>
+        <span class="meet__label"> نام و نام خانوادگی: </span>
         <span class="meet__value">
           {{ student.studentname }} {{ student.lastname }}
         </span>
       </div>
       <div class="meet__field i-flex">
-        <span class="meet__lable"> شماره پرونده: </span>
+        <span class="meet__label"> شماره پرونده: </span>
         <span class="meet__value">
           {{ student.docnumber }}
         </span>
@@ -18,15 +18,15 @@
       <div class="meet__more" @click="moreDetails">اطلاعات بیشتر ...</div>
     </div>
     <div class="meet__field i-flex-column">
-      <span class="meet__lable i-flex i-flex-align-center">
-        موضوعات موردعلاقه در دکتری:
+      <span class="meet__label i-flex i-flex-align-center">
+        موضوعات موردعلاقه در دکترا:
       </span>
       <div class="meet__input">
         <field-text-input theme="light" v-model="favphdfields" />
       </div>
     </div>
     <div class="meet__field i-flex-column">
-      <span class="meet__lable i-flex i-flex-align-center">
+      <span class="meet__label i-flex i-flex-align-center">
         برداشت اولیه:
       </span>
       <div class="meet__text-area">
@@ -38,9 +38,56 @@
       </div>
     </div>
     <div class="meet__field i-flex-column">
-      <span class="meet__lable i-flex i-flex-align-center"> توضیحات: </span>
+      <span class="meet__label i-flex i-flex-align-center"> توضیحات: </span>
       <div class="meet__text-area">
         <field-text-area-input height="100" theme="light" v-model="opinion" />
+      </div>
+    </div>
+
+    <span class="meet__label meet__table--label" v-if="comments.length">
+      نظرات ثبت شده:
+    </span>
+    <div class="meet__table i-flex-column" v-if="comments.length">
+      <div class="meet__table__row meet__table__row--header i-flex">
+        <div class="i-flex i-flex-align-center" style="flex: 1"></div>
+        <div class="i-flex i-flex-align-center" style="flex: 4">نام استاد</div>
+        <div class="i-flex i-flex-align-center" style="flex: 4">
+          برداشت اولیه
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 6">
+          موضوعات موردعلاقه دکترا
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 7">توضیحات</div>
+      </div>
+      <div
+        class="meet__table__row i-flex"
+        v-for="(comment, index) in comments"
+        :key="index"
+      >
+        <div class="i-flex i-flex-align-center" style="flex: 1">
+          {{ index + 1 }}
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 4">
+          {{ `${comment.user_id}`.substring(0, 20) }}
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 4">
+          <span v-b-tooltip.hover :title="comment.firstimpression">
+            {{ `${comment.firstimpression}`.substring(0, 16)
+            }}<span v-if="comment.firstimpression?.length > 16">...</span>
+          </span>
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 6">
+          <span v-b-tooltip.hover :title="comment.favphdfields">
+            {{ `${comment.favphdfields}`.substring(0, 25)
+            }}<span v-if="comment.favphdfields?.length > 25">...</span>
+          </span>
+        </div>
+        <div class="i-flex i-flex-align-center" style="flex: 7">
+          <span v-b-tooltip.hover :title="comment.opinion">
+            {{ `${comment.opinion}`.substring(0, 30)
+            }}<span v-if="comment.favphdfields?.length > 30">...</span>
+          </span>
+        </div>
       </div>
     </div>
     <div class="meet__buttons i-flex i-flex-justify-between">
@@ -132,17 +179,14 @@ export default {
   },
   created() {
     // this.user_id = Math.ceil(Math.random() * 5);
-    this.user_id = 2;
+    this.user_id = 1;
     if (this.docnumber) {
       this.$axios.get(`/student/${this.docnumber}`).then((response) => {
         this.student = response.data;
       });
       this.$axios.get(`comment/${this.docnumber}`).then((res) => {
-        this.comments = res.data;
-        let userComment = this.comments.find(
-          (el) => el.user_id == this.user_id
-        );
-
+        this.comments = res.data.filter((el) => el.user_id != this.user_id);
+        let userComment = res.data.find((el) => el.user_id == this.user_id);
         if (userComment) {
           this.firstimpression = userComment.firstimpression;
           this.opinion = userComment.opinion;
@@ -156,17 +200,13 @@ export default {
       this.showDetails = true;
     },
     submitComment() {
-      this.$axios
-        .post("/comment", {
-          firstimpression: this.firstimpression,
-          opinion: this.opinion,
-          favphdfields: this.favphdfields,
-          user_id: this.user_id,
-          docnumber: this.docnumber,
-        })
-        .then(() => {
-          this.$emit("close");
-        });
+      this.$axios.post("/comment", {
+        firstimpression: this.firstimpression,
+        opinion: this.opinion,
+        favphdfields: this.favphdfields,
+        user_id: this.user_id,
+        docnumber: this.docnumber,
+      });
     },
   },
 };
@@ -175,7 +215,7 @@ export default {
 <style lang="scss" scoped>
 .meet {
   padding: 32px 32px;
-  max-width: 1000px;
+  min-width: 825px;
   min-height: 500px;
   max-height: 90vh;
   overflow: hidden auto;
@@ -186,7 +226,7 @@ export default {
       margin-left: 110px;
     }
   }
-  &__lable {
+  &__label {
     margin-left: 24px;
     font-size: fontSize("lg");
     font-weight: 500;
@@ -217,6 +257,7 @@ export default {
   }
   &__buttons {
     margin: 0 25%;
+    margin-top: 32px;
   }
   &__back {
     margin-left: 0 !important;
@@ -246,6 +287,58 @@ export default {
       & > span:first-child {
         font-weight: 700;
         font-size: fontSize("l");
+      }
+    }
+  }
+  &__table {
+    width: 100%;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    border-collapse: collapse;
+    border-radius: 8px;
+    font-size: 14px;
+    margin-top: 8px;
+    &--label {
+      margin-top: 32px;
+    }
+    &__row {
+      span {
+        color: var(--color-3);
+      }
+      padding: 0 12px;
+      background-color: var(--color-2);
+      &--header {
+        border-top: solid 1px var(--on-color-3);
+        background-color: var(--color-3);
+        border-top-left-radius: 2px;
+        border-top-right-radius: 2px;
+        color: var(--on-color-3);
+        padding: 0 12px;
+        font-size: 16px;
+      }
+      height: 50px;
+      border-bottom: 1px solid var(--color-2);
+      &:last-child {
+        border-bottom: 2px solid var(--on-color-3);
+      }
+      &:nth-of-type(even) {
+        background-color: var(--color-1);
+      }
+      &:hover {
+        text-shadow: var(--highlight-color) 1px 1px 2px;
+      }
+    }
+    &-icon {
+      cursor: pointer;
+      width: 20px;
+      &:hover {
+        width: 22px;
+      }
+    }
+    &-more {
+      cursor: pointer;
+      width: 15px;
+      &:hover {
+        width: 17px;
       }
     }
   }
