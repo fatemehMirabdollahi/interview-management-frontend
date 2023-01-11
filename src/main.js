553f867ead72c375c2e495834c8b975bf3925a77
fmirabdollahi <f.mirabdollahi@samayesh.com>
2023-01-11 23:12:19 +0330

    add bootstrap icon

--- src/main.js
+++ src/main.js
@@ -7,13 +7,25 @@ import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import vClickOutside from "click-outside-vue3";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+/* add icons to the library */
+library.add(faUserSecret);
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
+
 app
   .use(store)
   .use(router)
   .use(BootstrapVue3)
   .use(vClickOutside)
   .use(require("vue3-jalali-moment"))
+  .component("font-awesome-icon", FontAwesomeIcon)
+
   .mount("#app");
