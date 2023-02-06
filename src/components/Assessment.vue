16a29912c5e8f3d4c566d2d01c9e923847782794
fmirabdollahi <f.mirabdollahi@samayesh.com>
2023-02-07 00:45:04 +0330

    fix print assessment

--- src/components/Assessment.vue
+++ src/components/Assessment.vue
@@ -45,7 +45,8 @@
     v-if="showForm"
   >
     <div class="i-flex-column i-flex-align-center assessment__form">
-      <div class="i-flex-column i-width-1-1">
+      <div class="i-flex-column i-width-1-1" style="position: relative">
+        <i class="bi bi-file-earmark-pdf pdf-icon" @click="download"></i>
         <span class="assessment__form-header"
           >۱) نمره سوابق آموزشی، پژوهشی و فناوری</span
         >
@@ -281,6 +282,226 @@
       </div>
     </div>
   </modal-window>
+  <vue3-html2pdf
+    ref="html2Pdf"
+    :manual-pagination="true"
+    :enable-download="true"
+    :filename="`assessment__${selectedDocnumber}`"
+    pdf-format="a4"
+  >
+    <template v-slot:pdf-content>
+      <div class="print i-flex-column">
+        <span class="print--h1">فرم پیشنهادی شماره (۲)</span>
+        <span class="print--h2"
+          >نحوه بررسی سوابق علمی و مصاحبه آزمون دکتری (Ph.D)</span
+        >
+        <span class="print--desc"
+          >براساس قانون سنجش و پذیرش دانشجو در دوره های تحصیلات تکمیلی مصاحبه و
+          بررسی سوابق آموزشی، پژوهشی و فناوری به شرح زیر است.</span
+        >
+        <div class="print__details i-flex i-flex-column">
+          <span class="print__details--title">دکتری آموزشی - پژوهشی:</span>
+          <div class="i-flex i-width-1-1 i-flex-justify-between">
+            <div class="print__details__item">
+              <span>۱-</span
+              ><span>سوابق آموزشی، پژوهشی و فناوری (۲۰ نمره)</span>
+            </div>
+            <div class="print__details__item">
+              <span>۱-</span
+              ><span>سوابق آموزشی، پژوهشی و فناوری (۳۰ نمره)</span>
+            </div>
+          </div>
+        </div>
+        <div class="print__warning">
+          <span>تذکر ۱:</span>
+          <span
+            >نمره تمامی داوطلبان در هر یک از بخش های «سوابق آموزشی، پژوهشی و
+            فناوری، مصاحبه علمی و سنجش علمی» توسط موسسه به صورت جداگانه از طریق
+            پرتال ارتباطی به سازمان اعلام خواهد شد.</span
+          >
+        </div>
+        <span class="print__form--title">دکتری آموزشی-پژوهشی</span>
+        <div>
+          <div class="i-flex-column i-width-1-1" style="position: relative">
+            <span class="print__form-header"
+              >۱) نمره سوابق آموزشی، پژوهشی و فناوری</span
+            >
+            <span class="print__form-desc"
+              >جدول ۱- نحوه مساحبه نمرات آموزشی، پژوهشی و فناوری</span
+            >
+            <div
+              class="i-flex-column i-flex-align-center i-flex-justify-center print__form__table"
+            >
+              <div
+                class="print__form__table__row print__form__table__row--header i-flex"
+              >
+                <div class="i-flex i-flex-align-center" style="flex: 1">
+                  ردیف
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 12">
+                  نوع فعالیت
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 3">
+                  حداکثر نمره
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">
+                  نحوه ارزیابی
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 3">
+                  نمره مکتسبه
+                </div>
+              </div>
+              <div
+                class="print__form__table__row i-flex"
+                v-for="(row, index) in form_1"
+                :key="index"
+              >
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 1"
+                >
+                  {{ index }}
+                </div>
+                <div
+                  class="i-flex-column i-flex-justify-center"
+                  style="flex: 12"
+                >
+                  <span v-for="title in row.title" :key="title">{{
+                    title
+                  }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 3"
+                >
+                  <span>{{ row.max }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 4"
+                >
+                  <span>{{ row.how }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 3"
+                  :style="[index == 0 ? 'flex-direction: column' : '']"
+                >
+                  <span v-if="index != 0"> {{ row.grade[0] }}</span>
+                  <span v-if="index == 0"> {{ row.grade[0] }}</span>
+                  <span v-if="index == 0"> {{ row.grade[1] }}</span>
+                  <span v-if="index == 0"> {{ row.grade[2] }}</span>
+                </div>
+              </div>
+              <div
+                class="print__form__table__row print__form__table__row--footer i-flex"
+              >
+                <div class="i-flex i-flex-align-center" style="flex: 1"></div>
+                <div class="i-flex i-flex-align-center" style="flex: 12">
+                  جمع
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 3">
+                  {{ maxValueSum_1 }}
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">-</div>
+                <div class="i-flex i-flex-align-center" style="flex: 3">
+                  {{ gradeSum_1 }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="i-flex-column i-width-1-1">
+            <span class="print__form-header"
+              >۲) نمره مصاحبه علمی و سنجش علمی</span
+            >
+            <span class="print__form-desc"
+              >جدول ۲-نمره مصاحبه علمی و سنجش علمی</span
+            >
+            <div
+              class="i-flex-column i-flex-align-center i-flex-justify-center print__form__table"
+            >
+              <div
+                class="print__form__table__row print__form__table__row--header i-flex"
+              >
+                <div class="i-flex i-flex-align-center" style="flex: 1">
+                  ردیف
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 7">
+                  نوع فعالیت
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">
+                  حداکثر نمره
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 5">
+                  نحوه ارزیابی
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">
+                  نمره مکتسبه
+                </div>
+              </div>
+              <div
+                class="print__form__table__row i-flex"
+                v-for="(row, index) in form_2"
+                :key="index"
+              >
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 1"
+                >
+                  {{ index }}
+                </div>
+                <div
+                  class="i-flex-column i-flex-justify-center i-flex-align-center"
+                  style="flex: 7"
+                >
+                  <span v-for="title in row.title" :key="title">{{
+                    title
+                  }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 4"
+                >
+                  <span>{{ row.max }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 5"
+                >
+                  <span>{{ row.how }}</span>
+                </div>
+                <div
+                  class="i-flex i-flex-align-center i-flex-justify-center"
+                  style="flex: 4"
+                >
+                  <span>{{ row.grade[0] }}</span>
+                </div>
+              </div>
+              <div
+                class="print__form__table__row print__form__table__row--footer i-flex"
+              >
+                <div class="i-flex i-flex-align-center" style="flex: 1"></div>
+                <div class="i-flex i-flex-align-center" style="flex: 7">
+                  جمع
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">
+                  {{ maxValueSum_2 }}
+                </div>
+                <div class="i-flex i-flex-align-center" style="flex: 5">-</div>
+                <div class="i-flex i-flex-align-center" style="flex: 4">
+                  {{ gradeSum_2 }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="print__footer i-flex i-flex-justify-even">
+          <span>نام و نام خانوادگی مصاحبه کننده:</span>
+          <span>تاریخ و امضاءمصاحبه کننده:</span>
+        </div>
+      </div>
+    </template>
+  </vue3-html2pdf>
 </template>
 
 <script>
@@ -289,8 +510,10 @@ import Input from "./FieldTextInput";
 import Button from "./FormButton";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import Vue3Html2pdf from "vue3-html2pdf";
+
 export default {
-  components: { ModalWindow, Input, Button },
+  components: { ModalWindow, Input, Button, Vue3Html2pdf },
   data() {
     return {
       showForm: false,
@@ -342,6 +565,9 @@ export default {
     if (this.interviewYear) this.getInterviewStudents();
   },
   methods: {
+    download() {
+      this.$refs.html2Pdf.generatePdf();
+    },
     getInterviewStudents() {
       this.$axios.get(`/interview/ ${this.interviewYear}`).then((response) => {
         this.students = response.data;
@@ -575,6 +801,164 @@ export default {
       width: 25%;
       margin-top: 16px;
     }
+  }
+}
+.pdf-icon {
+  position: absolute;
+  left: 112px;
+  font-size: 30px;
+  cursor: pointer;
+  &:hover {
+    text-shadow: var(--highlight-light-color) 1.95px 1.95px 2.6px;
+  }
+}
+$page-height: 295mm;
+$page-width: 209mm;
+.print {
+  width: $page-width;
+  height: $page-height;
+  max-height: $page-height;
+  padding: 25px 25px;
+  overflow: hidden;
+  font-size: 12px;
+  direction: rtl;
+  &--h1 {
+    font-size: 12px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  &--h2 {
+    font-size: 12px;
+    font-weight: bold;
+    align-self: center;
+    margin-bottom: 20px;
+  }
+  &--desc {
+    font-size: 11px;
+    padding-bottom: 10px;
+  }
+  &__details {
+    width: 100%;
+    border: black solid 1px;
+    padding: 10px;
+    margin-bottom: 5px;
+    &--title {
+      font-size: 12px;
+      font-weight: bold;
+    }
+    &__item {
+      padding: 0 10px;
+      &:last-child {
+        margin-left: 100px;
+      }
+      & > span {
+        font-size: 10px;
+      }
+    }
+  }
+  &__warning {
+    margin-bottom: 10px;
+    & > span {
+      font-size: 11px;
+    }
+    & > span:first-child {
+      font-weight: bold;
+    }
+  }
+  &__form {
+    &--title {
+      font-size: 12px;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    overflow-y: scroll;
+    width: 1200px;
+    min-height: 500px;
+    padding: 32px;
+    &-header {
+      font-size: 12px;
+      font-weight: 600;
+      margin-bottom: 8px;
+    }
+    &-desc {
+      font-size: 11;
+      margin-bottom: 32px;
+      font-style: italic;
+    }
+    &__table {
+      width: 100%;
+      margin-bottom: 16px;
+      &__row {
+        width: 80%;
+        min-height: 30px;
+        border-bottom: 1px solid black;
+        border-right: 1px solid black;
+        border-left: 1px solid black;
+        padding: 0 8px;
+        font-size: 10;
+        & > div {
+          border-left: 1px solid black;
+          &:first-child {
+            border-left: none;
+          }
+          &:last-child {
+            border-left: none;
+          }
+          & > span {
+            margin-bottom: 8px;
+            &:last-child {
+              margin-bottom: 0;
+            }
+          }
+          &:first-child {
+            font-weight: 700;
+          }
+        }
+        &--header {
+          & > div {
+            display: flex;
+            align-content: center;
+            justify-content: center;
+            border-left: 1px solid black;
+            &:first-child {
+              border-left: none;
+            }
+            &:last-child {
+              border-left: none;
+            }
+          }
+          background-color: rgb(226, 225, 225);
+          border: 1px solid black;
+          font-size: 10px;
+          font-weight: 500;
+        }
+        &--footer {
+          & > div {
+            display: flex;
+            align-content: center;
+            justify-content: center;
+            border-left: 1px solid black;
+            &:first-child {
+              border-left: none;
+            }
+            &:last-child {
+              border-left: none;
+            }
+          }
+          background-color: rgb(226, 225, 225);
+          border: 1px solid black;
+          border-top: none;
+          color: black;
+          font-size: 10px;
+          // min-height: 55px !important;
+          font-weight: 500;
+        }
+      }
+    }
+  }
+  &__footer {
+    font-size: 12px;
+    margin-top: 20px;
   }
 }
 </style>
